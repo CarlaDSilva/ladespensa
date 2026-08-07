234ecb021945168d38a6676d69d64851f1be374e
Carla da Silva <131472395+CarlaDSilva@users.noreply.github.com>
2026-08-07 13:59:29 +0200

    Update cache version to ladespensa-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ladespensa-v1.5.1';
+const CACHE = 'ladespensa-v2';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
