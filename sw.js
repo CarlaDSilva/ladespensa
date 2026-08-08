bfc3a703078ed9c226b94408bcfa566d7df5525b
Carla da Silva <131472395+CarlaDSilva@users.noreply.github.com>
2026-08-08 12:28:49 +0200

    sw.js: bump cache to ladespensa-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ladespensa-v2';
+const CACHE = 'ladespensa-v3';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
